489750287bdebf9c573feab43aed93a246876a0a
Venkat2315 <tennetihemant@gmail.com>
2026-04-19 23:43:38 +0530

    initial-commit

---
+++ src/main/java/com/personalProject/code_reviewer/CodeReviewerApplication.java
@@ -0,0 +1,13 @@
+package com.personalProject.code_reviewer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeReviewerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeReviewerApplication.class, args);
+	}
+
+}
