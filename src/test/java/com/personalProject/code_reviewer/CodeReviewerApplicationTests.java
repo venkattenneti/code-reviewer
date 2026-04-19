f059aba391aa503c0df909b85a3f82a4e8670df1
Venkat2315 <tennetihemant@gmail.com>
2026-04-19 23:43:38 +0530

    initial-commit

---
+++ src/test/java/com/personalProject/code_reviewer/CodeReviewerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.personalProject.code_reviewer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeReviewerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
